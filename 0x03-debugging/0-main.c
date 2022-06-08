6b2702330b31015b03cd38d8be0e541d59d53a10
Surina-Singh <surinsingh@hotmail.co.za>
2022-06-08 03:35:32 -0700

    0-main.c, main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
-
 int main(void)
 {
-        int i;
+        int n;
 
-        i = 98;
-        positive_or_negative(i);
+        n =  rand() - RAND_MAX / 2;
+        positive_or_negative(n);
 
         return (0);
 }
